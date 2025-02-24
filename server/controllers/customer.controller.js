b30e3b3872e259bae87b42781f07f7414cc8c228
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-24 23:33:17 +0500

    remove bugs

--- server/controllers/customer.controller.js
+++ server/controllers/customer.controller.js
@@ -4,15 +4,19 @@ import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 // Create a new customer
-export const createCustomer = asyncHandler(async (req, res ) => {
+export const createCustomer = asyncHandler(async (req, res) => {
   const { fullname, email, mobile } = req.body;
+  const existingCustomer = await Customer.findOne({ email });
+  if (existingCustomer) {
+    throw new ApiError(409, "Email already exists");
+  }
   const customer = new Customer({ fullname, email, mobile });
   await customer.save();
   res.status(201).json(new ApiResponse(201, customer, 'Customer created successfully'));
 });
 
 // Fetch all customers
-export const fetchCustomer = asyncHandler(async (_, res ) => {
+export const fetchCustomer = asyncHandler(async (_, res) => {
   const customers = await Customer.find();
   res.status(200).json(new ApiResponse(200, customers, 'Customers fetched successfully'));
 });
@@ -22,17 +26,17 @@ export const fetchCustomerById = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const customer = await Customer.findById(id);
   if (!customer) {
-    return next(new ApiError(404, 'Customer not found'));
+    throw new ApiError(409, "customer not found")
   }
   res.status(200).json(new ApiResponse(200, customer, 'Customer fetched successfully'));
 });
 
 // Delete a customer
-export const deleteCustomer = asyncHandler(async (req, res ) => {
+export const deleteCustomer = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const customer = await Customer.findByIdAndDelete(id);
   if (!customer) {
-    return next(new ApiError(404, 'Customer not found'));
+    throw new ApiError(409, "customer not found")
   }
   res.status(200).json(new ApiResponse(200, customer, 'Customer deleted successfully'));
 });
@@ -41,9 +45,9 @@ export const deleteCustomer = asyncHandler(async (req, res ) => {
 export const updateCustomer = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const { fullname, email, mobile } = req.body;
-  const customer = await Customer.findByIdAndUpdate(id, { fullname, email, mobile }, { new: true});
+  const customer = await Customer.findByIdAndUpdate(id, { fullname, email, mobile }, { new: true });
   if (!customer) {
-    return next(new ApiError(404, 'Customer not found'));
+    throw new ApiError(409, "customer not found")
   }
   res.status(200).json(new ApiResponse(200, customer, 'Customer updated successfully'));
 });
