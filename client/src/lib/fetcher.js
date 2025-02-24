29f23f652fdcaf63bbfe3c2f4d8b613f9dd498a3
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-24 17:06:45 +0500

    Add axios fetcher utility for API requests

---
+++ client/src/lib/fetcher.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080';
+
+const fetcher = async (url) => {
+  try {
+    const response = await axios.get(url);
+    return response.data.data; 
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default fetcher;
